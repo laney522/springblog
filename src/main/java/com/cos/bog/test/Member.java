465d24b2c2fa84bdb58b3f3b31db7f5fe0223179
laney522 <pm72222@gmail.com>
2022-04-30 23:25:58 +0900

    add lombok settings

---
+++ src/main/java/com/cos/bog/test/Member.java
@@ -0,0 +1,16 @@
+package com.cos.bog.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+// @Getter
+// @Setter
+@Data
+//@AllArgsConstructor
+
+public class Member {
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+}
