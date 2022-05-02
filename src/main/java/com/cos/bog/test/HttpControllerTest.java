811b5f5d4d4bc6616a1eaa24140ba5e0ee8a4d0d
laney522 <pm72222@gmail.com>
2022-05-02 23:27:31 +0900

    Get Mapping_lombokTest

--- src/main/java/com/cos/bog/test/HttpControllerTest.java
+++ src/main/java/com/cos/bog/test/HttpControllerTest.java
@@ -14,6 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpControllerTest {
 
+	private static final String TAG="HttpControllerTest:";
+	
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		Member m = new Member(1,"ssar","1234","email");
+		System.out.println(TAG+"getter"+m.getId());
+		m.setId(5000);
+		System.out.println(TAG+"setter"+m.getId());
+		return "lombok test 완료";
+	}
+	
 	// https://localhost:8080/http/post (select)
 	@GetMapping("/http/get")
 	public String getTest(Member m) {
